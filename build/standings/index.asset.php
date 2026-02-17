da61ba1b763c6684a76473c9b14b6ce030ad796a
Dan Fisher <dan.fisher85@yahoo.com>
2026-02-17 15:50:31 +0100

    Standings #4

--- build/standings/index.asset.php
+++ build/standings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd0a36b9ff4369e618e51');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c64bc13ba92d227d4267');
--- src/standings/index.js
+++ src/standings/index.js
@@ -20,12 +20,150 @@ registerBlockType('matchday/standings', {
 					</PanelBody>
 				</InspectorControls>
 				<div {...blockProps}>
-					<Placeholder
-						icon="list-view"
-						label={__('Tournament Standings', 'matchday-blocks')}
-					>
-						{__('This block will display tournament standings from MeinTurnierplan.', 'matchday-blocks')}
-					</Placeholder>
+					<div className="matchday-standings">
+						<div className="matchday-standings__group">
+							<h3 className="matchday-standings__group-heading">Group A</h3>
+							<div className="matchday-standings__table-wrapper">
+								<table>
+									<thead>
+										<tr>
+											<th className="matchday-table__pos">Pl</th>
+											<th className="matchday-table__participant">Participants</th>
+											<th>M</th>
+											<th>G</th>
+											<th>GD</th>
+											<th className="matchday-table__pts">Pts</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td className="matchday-table__pos">1st</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 1
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">2nd</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 2
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">3rd</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 3
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">4th</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 4
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<div className="matchday-standings__group">
+							<h3 className="matchday-standings__group-heading">Group B</h3>
+							<div className="matchday-standings__table-wrapper">
+								<table>
+									<thead>
+										<tr>
+											<th className="matchday-table__pos">Pl</th>
+											<th className="matchday-table__participant">Participants</th>
+											<th>M</th>
+											<th>G</th>
+											<th>GD</th>
+											<th className="matchday-table__pts">Pts</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td className="matchday-table__pos">1st</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 1
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">2nd</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 2
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">3rd</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 3
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+										<tr>
+											<td className="matchday-table__pos">4th</td>
+											<td className="matchday-table__participant">
+												<div className="matchday-table__participant-inner">
+													<div className="matchday-table__participant-logo-placeholder"></div>
+													Team Name 4
+												</div>
+											</td>
+											<td>0</td>
+											<td>0:0</td>
+											<td>0</td>
+											<td className="matchday-table__pts">0</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
 				</div>
 			</>
 		);
